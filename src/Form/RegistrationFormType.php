909d72860d6616b797a2025d098cc76e380520e1
steph-GoodLife <stephripa@gmail.com>
2022-11-29 14:15:59 +0100

    màj tailwind

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\CallbackTransformer;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -19,7 +20,14 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
+            ->add('email', EmailType::class, [
+                'attr' => [ 'class' => 
+                        'relative block w-full appearance-none rounded-none 
+                        rounded-t-md border border-gray-300 px-3 py-2 
+                        text-gray-900 placeholder-gray-500 focus:z-10 
+                        focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm'
+                ]
+            ])
             ->add('roles', ChoiceType::class, [
                 'required' => true,
                 'multiple' => false,
@@ -29,6 +37,12 @@ class RegistrationFormType extends AbstractType
                   'Professeur' => 'ROLE_PROF',
                   'Direction' => 'ROLE_DIRECT',
                 ],
+                'attr' => [ 'class' => 
+                        'relative block w-full appearance-none rounded-none 
+                        rounded-t-md border border-gray-300 px-3 py-2 
+                        text-gray-900 placeholder-gray-500 focus:z-10 
+                        focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm'
+                ]
             ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
@@ -53,6 +67,11 @@ class RegistrationFormType extends AbstractType
                         'max' => 4096,
                     ]),
                 ],
+                'attr' => [ 'class' => 
+                        'relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 
+                        focus:z-10 focus:border-indigo-500 focus:outline-none 
+                        focus:ring-indigo-500 sm:text-sm'
+                ]
             ])
         ;
 
